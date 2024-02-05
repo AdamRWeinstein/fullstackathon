51a7e5f3522e30c0317eb626c83036fcdabf5dc2
gourranz <Zakaria.Gourrane@lions.enc.edu>
2024-02-05 10:54:26 -0500

    axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,25 @@
-import { useState, useEffect } from 'react'
-import './App.css'
-import {Routes, Route} from 'react-router-dom'
-import NavBar from './components/NavBar'
+import { useState, useEffect } from 'react';
+import './App.css';
+import {Router, Route, Routes } from 'react-router-dom'; 
+
+import Home from './components/Home';
+import About from './components/About';
 
 function App() {
-
   return (
-   
-      <div>
+    <div>
       <header></header>
-      <main>
-      <NavBar />
-      </main>
-      <footer></footer>
-
+          <main>
+    <Routes>   
+        <Route>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<Home />} />
+        </Route>
+      
+    </Routes>
+    </main>
     </div>
-  
-  )
+  );
 }
 
-export default App
+export default App;
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 
-export const BASE_URL = ''
+export const BASE_URL = 'https://naez-react-rides-b9658734aee9.herokuapp.com/'
 
 const Client = axios.create({baseURL: BASE_URL})
 
